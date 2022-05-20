10320d9f1ecfb2fb8e282941bb7378c1944a4203
Eyob <oddjobera@gmail.com>
2022-05-20 04:53:53 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,16 +7,16 @@
  */
 size_t print_list(const list_t *h)
 {
-        size_t ele = 0;
+	size_t ele = 0;
 
-        while (h)
-        {
-                if (!h->str)
-                        printf("[0] (nil)\n");
-                else
-                        printf("[%u] %s\n", h->len, h->str);
-                h = h->next;
-                ele++;
-        }
-        return (ele);
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		ele++;
+	}
+	return (ele);
 }
