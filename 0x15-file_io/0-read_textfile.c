596f34e87e17033e08b2fea64253f194745858b6
Eyob <oddjobera@gmail.com>
2022-05-30 02:55:58 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,9 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * read_textfile - reads a text file and prints the letters
  * @filename: filename.
- * @letters: numbers of letters printed.
- *
+ * @letters: printed numbers of letters.
  * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
