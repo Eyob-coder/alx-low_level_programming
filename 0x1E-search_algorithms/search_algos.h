055ee20b689b485005afdaf0d4244f32cbcfce04
Eyob-coder <oddjobera@gmail.com>
2022-11-02 04:10:40 -0700

    linear and binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,7 +7,7 @@
 
 /* Prototype */
 
-int linear_serach(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
 #endif
