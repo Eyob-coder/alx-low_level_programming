5ebb0f979cc44f0fbcbbc99c23dbf01825ec7998
Eyob-coder <oddjobera@gmail.com>
2022-11-02 04:02:10 -0700

    readme and prototype

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* Libraries */
+#include <stdio.h>
+#include <string.h>
+
+/* Prototype */
+
+int linear_serach(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
